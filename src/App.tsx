9743a5e815a308a4119aa3d714b93be21cd995ba
MattAKeider <kderr41@gmail.com>
2025-03-24 10:56:07 -0400

    add temp rule ignore for type error

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import MainContent from './components/MainContent/MainContent';
 import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
-import notesData, { Note } from './data/data';
+import notesData from './data/data';
 import getAllTags from './utility/helper';
 import classes from './App.module.css';
 
 const App = () => {
   const [notes, setNotes] = useState(notesData);
-
-  function handleAddNotes(note: Note) {
-    setNotes(prevNotes => [note, ...prevNotes]);
-  }
 
   return (
     <div className={classes.wrapper}>

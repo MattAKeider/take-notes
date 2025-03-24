cf233b48729384897787c8175b367d1f35c83e27
MattAKeider <kderr41@gmail.com>
2025-03-24 09:54:59 -0400

    fixed navigationbar test

--- tests/components/NavigationBar.test.tsx
+++ tests/components/NavigationBar.test.tsx
@@ -2,9 +2,11 @@ import { render } from '@testing-library/react';
 
 import NavigationBar from '../../src/components/NavigationBar/NavigationBar';
 
+const tagsData = ['TypeScript', 'React', 'Home', 'Hobbies'];
+
 describe('NavigationBar', () => {
   test('should render the NavigationBar component', () => {
-    const { container } = render(<NavigationBar />);
+    const { container } = render(<NavigationBar tags={tagsData} />);
     expect(container.querySelector('svg')).toBeInTheDocument();
   });
 });

4459354841dc7eb9238b369562e52a39653e41dc
MattAKeider <kderr41@gmail.com>
2025-05-05 10:09:55 -0400

    added note actions

--- src/components/NoteActions/NoteActions.tsx
+++ src/components/NoteActions/NoteActions.tsx
@@ -1,8 +1,17 @@
+import { 
+  MdOutlineArchive as Archive, 
+  MdDeleteOutline as Delete 
+} from 'react-icons/md';
+
+import Button from '../../ui/Button/Button';
 import classes from './NoteActions.module.css';
 
 const NoteActions = () => {
   return (
-    <p>Note Actions</p>
+    <div className={classes.wrapper}>
+      <Button className={classes.button} Icon={Archive}>Archive Note</Button>
+      <Button className={classes.button} Icon={Delete}>Delete Note</Button>
+    </div>
   );
 };
 
